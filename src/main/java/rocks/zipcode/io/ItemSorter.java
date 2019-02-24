2bad641556e292e7b31aece63f0c108e616d7077
Swathi Bikumalia <swathibikumalia@zipcoders-MBP.hsd1.pa.comcast.net>
2019-02-23 22:48:56 -0500

    tested

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,28 @@
 package rocks.zipcode.io;
 
-import java.util.Comparator;
+import java.util.*;
+
+import static java.util.Objects.compare;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+
+    List<Item> itemList = new ArrayList<Item>();
+
     public ItemSorter(Item[] items) {
+
+        this.itemList =Arrays.asList(items);
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Collections.sort(itemList ,comparator);
+        Item[] resultList = new Item[itemList.size()];
+     resultList = itemList.toArray(resultList);
+     return resultList;
+
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,15 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+    public int compare(Item e ,Item f) {
+        return e.getPrice().compareTo(f.getPrice());
+    }
 }
